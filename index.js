79c20ca911af723dce7c2f0cb65b9dc03a7a8110
Alessandro <Alessandro.godino2004@gmail.com>
2023-01-30 00:09:56 +1100

    HTML and CSS complete, JavaScript beginning

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// SLIDER
+var slider = document.getElementById('slider')
+var output = document.getElementById('year');
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+// MOVIE API - CALLING
 // https://www.omdbapi.com/?i=tt3896198&apikey=2ac18e86
+
